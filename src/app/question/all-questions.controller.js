3c60e29fe875dab0f9021498c01f18fd404e989a
knikolic <kristina.nikolic3@gmail.com>
2015-07-02 20:29:30 +0200

    part 2 second commit

--- src/app/question/all-questions.controller.js
+++ src/app/question/all-questions.controller.js
@@ -9,6 +9,46 @@ angular.module('poll')
 				$scope.title = 'Failed';
 			});
 			
+		addAtributes();
 		
+		function addAtributes() {
+			for (item in $scope.items) {
+				
+				item.totalA = 0;
+				item.totalB = 0;
+				item.totalC = 0;
+				
+				item.winner = function() {
+					
+					var sum = item.totalA + item.totalB + item.totalC;
+					if (sum == 0) {
+						return '';
+					};
+					
+					if (item.totalA > item.totalB && item.totalA > item.totalC) {
+						return item.answers[0].answer;
+					} else if (item.totalB > item.totalA && item.totalB > item.totalC) {
+						return item.answers[1].answer;
+					} else if (item.total > item.totalA && item.totalC > item.totalB) {
+						return item.answers[2].answer;
+					} else {
+						return "Answer tie!";	
+					};
+				};
+				
+				item.optionA = function() {
+					item.totalA = item.totalA + 1;
+				};
+				
+				item.optionB = function() {
+					item.totalB = item.totalB + 1;
+				};
+				
+				item.optionC = function() {
+					item.totalC = item.totalC + 1;
+				};
+				
+					}
+		}
 			
 	}]);
